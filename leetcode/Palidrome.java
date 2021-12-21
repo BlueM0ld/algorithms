45a27ba22f2469e9adc9d1973923698609e2506e
BlueM0ld <blu3M0ld@gmail.com>
2021-12-21 00:41:42 +0000

    add solution to leetcode problem isPalidrome

---
+++ leetcode/Palidrome.java
@@ -0,0 +1,15 @@
+public class Palidrome {
+    
+    public boolean isPalindrome(int x) {
+        
+        String number = Integer.toString(x);
+        
+        String reverse = new StringBuffer(number).reverse().toString();
+                
+        if(reverse.equals(number)){return true;}
+        
+        return false;
+        
+        
+    }
+}
