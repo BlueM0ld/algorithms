ee5681bba93fe4b42d557f276980c5085d61ece2
BlueM0ld <blu3M0ld@gmail.com>
2021-12-28 01:29:53 +0000

    add solution to leetcode problem Middle of the Linked List

---
+++ leetcode/MiddleOfLinkedList.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+public class MiddleOfLinkedList {
+    public ListNode middleNode(ListNode head) {
+        
+        ListNode tmp = head;
+        int len = 1;
+        
+        while(head.next!=null){
+            
+            len++;
+            head = head.next;
+        }
+        
+        int mid = len/2;
+        
+        for(int i =0; i<mid; i++){
+            
+            tmp = tmp.next;
+        }
+        
+        return tmp;
+    }
+}
