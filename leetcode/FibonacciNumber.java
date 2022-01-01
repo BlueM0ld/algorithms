43cc91ec964954747b3f0d19fa00e6f15a2370e5
BlueM0ld <blu3M0ld@gmail.com>
2022-01-01 19:33:03 +0000

    add solution to leetcode problem Fibonacci Number

---
+++ leetcode/FibonacciNumber.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class FibonacciNumber {
+    public int fib(int n) {
+        if(n == 0){return 0;}
+        if(n == 1){return 1;}
+        
+        int f = fib(n-1) + fib(n-2);
+        
+        
+        return f;
+        
+    }
+}
